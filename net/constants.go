2af701177fdc2cd14c2bf3cb71c6fd4f28666fd0
GabeCordo <gabe.cordovado@yahoo.ca>
2022-12-24 13:15:35 -0500

    net and string constants

---
+++ net/constants.go
@@ -0,0 +1,5 @@
+package net
+
+const (
+	LocalHost string = "127.0.0.1"
+)
---
+++ strings/constants.go
@@ -0,0 +1,7 @@
+package strings
+
+const (
+	EmptyString string = ""
+	NewLine     string = "\n" // more verbose (readable) than \n
+	Tab         string = "\t" // more verbose (readable) then \t
+)
